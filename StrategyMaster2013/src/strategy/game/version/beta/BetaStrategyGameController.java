57829491ee913dff53b5308a28d666667de227cf
scornman <spcornman@gmail.com>
2013-09-10 14:18:34 -0400

    Added BetaStrategyController file

---
+++ StrategyMaster2013/src/strategy/game/version/beta/BetaStrategyGameController.java
@@ -0,0 +1,31 @@
+package strategy.game.version.beta;
+
+import strategy.common.StrategyException;
+import strategy.game.StrategyGameController;
+import strategy.game.common.Location;
+import strategy.game.common.MoveResult;
+import strategy.game.common.Piece;
+import strategy.game.common.PieceType;
+
+public class BetaStrategyGameController implements StrategyGameController {
+
+	@Override
+	public void startGame() throws StrategyException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public MoveResult move(PieceType piece, Location from, Location to)
+			throws StrategyException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Piece getPieceAt(Location location) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
