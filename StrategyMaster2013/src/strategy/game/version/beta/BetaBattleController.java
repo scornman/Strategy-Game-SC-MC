bb1a2d50d87592e4a714339969c838e0ca806b3f
scornman <spcornman@gmail.com>
2013-09-15 14:44:10 -0400

    Implemented getWinningPiece method

--- StrategyMaster2013/src/strategy/game/version/beta/BetaBattleController.java
+++ StrategyMaster2013/src/strategy/game/version/beta/BetaBattleController.java
@@ -1,5 +1,8 @@
 package strategy.game.version.beta;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import strategy.common.PlayerColor;
 import strategy.game.common.Location;
 import strategy.game.common.MoveResult;
@@ -20,34 +23,90 @@ public class BetaBattleController {
 	BetaBoard gameBoard;
 	Location attackLocation;
 	Location defendLocation;
+	Map<PieceType, Integer> rankMap;
 
+	/**
+	 * Create a new BetaBattleController to manage a battle.
+	 * 
+	 * @param board
+	 *            the game board.
+	 * @param attackLocation
+	 *            the location that the attacking piece moved from.
+	 * @param defendLocation
+	 *            the location of the defending piece.
+	 */
 	public BetaBattleController(BetaBoard board, Location attackLocation,
 			Location defendLocation) {
 		gameBoard = board;
 		this.attackLocation = attackLocation;
 		this.defendLocation = defendLocation;
+		rankMap = setupPieceRanks();
 	}
 
+	/**
+	 * Sets up the relationships between the piece types and their ranks.
+	 * 
+	 * @return a map that maps piece type to rank.
+	 */
+	private Map<PieceType, Integer> setupPieceRanks() {
+		Map<PieceType, Integer> pieceRanks = new HashMap<PieceType, Integer>();
+		pieceRanks.put(PieceType.MARSHAL, 12);
+		pieceRanks.put(PieceType.COLONEL, 10);
+		pieceRanks.put(PieceType.CAPTAIN, 8);
+		pieceRanks.put(PieceType.LIEUTENANT, 7);
+		pieceRanks.put(PieceType.SERGEANT, 6);
+		pieceRanks.put(PieceType.FLAG, 1);
+		return pieceRanks;
+	}
+
+	/**
+	 * Determine the result of the battle.
+	 * 
+	 * @return the battle result.
+	 */
 	public MoveResult getBattleResult() {
 		Piece attackPiece = gameBoard.getPieceAt(attackLocation);
 		Piece defendPiece = gameBoard.getPieceAt(defendLocation);
 
-		// TODO: HACK!
-		Piece winningPiece = attackPiece;
+		Piece winningPiece = getWinningPiece(attackPiece, defendPiece);
 
-		MoveResultStatus status = MoveResultStatus.RED_WINS;
+		MoveResultStatus status = null;
 
-		return new MoveResult(status,
-				new PieceLocationDescriptor(attackPiece, defendLocation));
+		if (defendPiece.getType() == PieceType.FLAG) {
+			if (defendPiece.getOwner() == PlayerColor.BLUE) {
+				status = MoveResultStatus.RED_WINS;
+			} else {
+				status = MoveResultStatus.BLUE_WINS;
+			}
+		} else {
+			status = MoveResultStatus.OK;
+		}
 
-//		if (targetPiece.getOwner() == PlayerColor.BLUE) {
-//			// TODO: Remove Fake
-//
-//		} else if (targetPiece.getOwner() == PlayerColor.RED) {
-//			// TODO: Remove Fake
-//			return new MoveResult(MoveResultStatus.BLUE_WINS,
-//					new PieceLocationDescriptor(movingPiece, to));
-//		}
+		return new MoveResult(status, new PieceLocationDescriptor(attackPiece,
+				defendLocation));
 
+	}
+
+	/**
+	 * Determine which of two pieces wins the battle.
+	 * 
+	 * @param attackPiece
+	 *            the piece that is attacking.
+	 * @param defendPiece
+	 *            the piece that is defending.
+	 * @return the piece that wins the battle.
+	 */
+	private Piece getWinningPiece(Piece attackPiece, Piece defendPiece) {
+		int attackerRank = rankMap.get(attackPiece.getType());
+		int defenderRank = rankMap.get(defendPiece.getType());
+
+		if (attackerRank > defenderRank) {
+			return attackPiece;
+		} else if (defenderRank > attackerRank) {
+			return defendPiece;
+		} else {
+			// If the two pieces have the same rank, there is no battle winner.
+			return null;
+		}
 	}
 }
