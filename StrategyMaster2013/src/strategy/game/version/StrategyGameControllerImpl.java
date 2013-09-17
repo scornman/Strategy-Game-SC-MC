0505116a77fa1482567ca21eaa8e2a35a92f6a8b
Madalyn Coryea <madalyn@peachplex.com>
2013-09-17 09:48:28 -0400

    doing the concrete class

---
+++ StrategyMaster2013/src/strategy/game/version/StrategyGameControllerImpl.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package strategy.game.version;
+
+import strategy.common.StrategyException;
+import strategy.game.StrategyGameController;
+import strategy.game.common.Location;
+import strategy.game.common.MoveResult;
+import strategy.game.common.Piece;
+import strategy.game.common.PieceType;
+
+/**
+ * @author Madalyn
+ *
+ */
+public class StrategyGameControllerImpl implements StrategyGameController{
+
+	/**
+	 * 
+	 */
+	public StrategyGameControllerImpl() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void startGame() throws StrategyException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public MoveResult move(PieceType piece, Location from, Location to)
+			throws StrategyException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Piece getPieceAt(Location location) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
