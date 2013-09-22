e21bd59282ba9e7c785533b698e242a3bee71d91
Madalyn Coryea <madalyn@peachplex.com>
2013-09-21 23:20:32 -0400

    forgot to commit first, added check for not attacking own piece validator. more tests passing WOOOO

---
+++ StrategyMaster2013/src/strategy/game/version/validateMoveBehaviors/NotAttackingOwnTeamMoveValidator.java
@@ -0,0 +1,29 @@
+package strategy.game.version.validateMoveBehaviors;
+
+import strategy.common.PlayerColor;
+import strategy.game.common.Location;
+import strategy.game.common.Piece;
+import strategy.game.common.PieceType;
+import strategy.game.version.Board;
+import strategy.game.version.ValidateMoveBehavior;
+
+public class NotAttackingOwnTeamMoveValidator implements ValidateMoveBehavior{
+	private final Board gameBoard;
+	
+	public NotAttackingOwnTeamMoveValidator(Board gameBoard) {
+		this.gameBoard = gameBoard;
+	}
+
+	/**
+	 * makes sure that the piece the player is attacking is not its own team
+	 */
+	@Override
+	public boolean isMoveValid(PieceType piece, Location from, Location to) {
+		Piece attackingPiece = gameBoard.getPieceAt(from);
+		Piece defendingPiece = gameBoard.getPieceAt(to);
+		PlayerColor attackColor = attackingPiece.getOwner();
+		PlayerColor defendColor = defendingPiece.getOwner();
+		
+		return (attackColor != defendColor);
+	}
+}
